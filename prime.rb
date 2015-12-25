8050349342c72815db4d15470389fca9bdf89968
MGVera <madelinegvera@gmail.com>
2015-12-25 02:00:59 +0000

    nitrous-backup at Fri Dec 25 02:00:59 UTC 2015

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  while (num <= last)
+    prime_flag = true
+
+    x = 2
+
+    while (x <= num / 2)
+
+      if (num % x == 0)
+        prime_flag = false
+        break
+      end
+
+      x += 1
+end
